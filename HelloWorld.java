aa4b9d7282e3e50b97f54774dad7f541e6c63457
zebra <305737803@qq.com>
2017-11-03 10:05:00 +0800

    修改了Hello 函数

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld{
     public static void main(String[] arg){
         for (int i=0;i<2;i++){
-            System.out.println("Happy，So Happy..."+i);
+            System.out.println("Happy zebra,So Happy..."+i);
         }
     }
 }
